5d65b09816babb470438e650cd458b59f97c1142
Carlito9 <58082159+Carlito9@users.noreply.github.com>
2021-07-16 17:43:17 +0200

    Update image_util.py

--- datasets/image_util.py
+++ datasets/image_util.py
@@ -51,22 +51,20 @@ class DATA_LOADER(object):
         feature = matcontent['features'].T
         label = matcontent['labels'].astype(int).squeeze() - 1
         matcontent = sio.loadmat(opt.dataroot + "/" + opt.dataset + "/" + opt.class_embedding + "_splits.mat")
-        trainval_loc = matcontent['trainval_loc'].squeeze() - 1
-        train_loc = matcontent['train_loc'].squeeze() - 1
-        val_unseen_loc = matcontent['val_loc'].squeeze() - 1
-        test_seen_loc = matcontent['test_seen_loc'].squeeze() - 1
-        test_unseen_loc = matcontent['test_unseen_loc'].squeeze() - 1    
+          
 
         attribute = matcontent['att']
-        index1 = [137, 142, 271]
-        index2 =  [108, 200, 42]
+        #UNCOMMENT THE CODE FROM HERE IF YOU USE MERGING ATTRIBUTES
+       """ index1 = [137, 142, 271]    #CHANGE THIS ARRAY WITH THE FIRST 3 ATTRIBUTES YOU WANT TO MERGE
+        index2 =  [108, 200, 42]    #CHANGE THIS ARRAY WITH THE SECOND 3 ATTRIBUTES YOU WANT TO MERGE
         col1 = []
         col2 = []
         for i in index1:
             col1.append(attribute[i])
         for i in index2:
             col2.append(attribute[i])
-        attribute = np.delete(attribute, [137, 142, 271, 108, 200, 42], axis=0)
+        total=index1+index2
+        attribute = np.delete(attribute, total, axis=0)   
         col1 = np.array(col1)
         col2 = np.array(col2)
         mean1 = np.mean(col1, axis=0)
@@ -74,50 +72,19 @@ class DATA_LOADER(object):
         attribute = np.transpose(attribute)
         attribute = np.c_[attribute, mean1]
         attribute = np.c_[attribute, mean2]
-
-        self.attribute = torch.from_numpy(attribute).float()
+        attribute = attribute.T
+"""
+        #STOP UNCOMMENTING 
+        
+        #UNCOMMENT HERE FOR DELETING ATTRIBUTES
+        """
+        attribute = np.delete(attribute, [137, 142, 271, 108, 200, 42], axis=0) #CHANGE THIS ARRAY WITH THE ATTRIBUTES YOU WANT TO DELETE
+        
+        """
+        self.attribute = torch.from_numpy(attribute.T).float()
         self.attribute /= self.attribute.pow(2).sum(1).sqrt().unsqueeze(1).expand(self.attribute.size(0),self.attribute.size(1))
 
 
-        """
-        if not opt.validation:
-            if opt.preprocessing:
-                if opt.standardization:
-                    print('standardization...')
-                    scaler = preprocessing.StandardScaler()
-                else:
-                    scaler = preprocessing.MinMaxScaler()
-                
-                _train_feature = scaler.fit_transform(feature[trainval_loc])
-                _test_seen_feature = scaler.transform(feature[test_seen_loc])
-                _test_unseen_feature = scaler.transform(feature[test_unseen_loc])
-                self.train_feature = torch.from_numpy(_train_feature).float()
-                mx = self.train_feature.max()
-                self.train_feature.mul_(1/mx)
-                self.train_label = torch.from_numpy(label[trainval_loc]).long() 
-                self.test_unseen_feature = torch.from_numpy(_test_unseen_feature).float()
-                self.test_unseen_feature.mul_(1/mx)
-                self.test_unseen_label = torch.from_numpy(label[test_unseen_loc]).long() 
-                self.test_seen_feature = torch.from_numpy(_test_seen_feature).float() 
-                self.test_seen_feature.mul_(1/mx)
-                self.test_seen_label = torch.from_numpy(label[test_seen_loc]).long()
-
-            else:
-                self.train_feature = torch.from_numpy(feature[trainval_loc]).float()
-                self.train_label = torch.from_numpy(label[trainval_loc]).long() 
-                self.test_unseen_feature = torch.from_numpy(feature[test_unseen_loc]).float()
-                self.test_unseen_label = torch.from_numpy(label[test_unseen_loc]).long() 
-                self.test_seen_feature = torch.from_numpy(feature[test_seen_loc]).float() 
-                self.test_seen_label = torch.from_numpy(label[test_seen_loc]).long()
-        else:
-            self.train_feature = torch.from_numpy(feature[train_loc]).float()
-            self.train_label = torch.from_numpy(label[train_loc]).long()
-            self.test_unseen_feature = torch.from_numpy(feature[val_unseen_loc]).float()
-            self.test_unseen_label = torch.from_numpy(label[val_unseen_loc]).long() 
-
-        self.seenclasses = torch.from_numpy(np.unique(self.train_label.numpy()))
-        self.unseenclasses = torch.from_numpy(np.unique(self.test_unseen_label.numpy()))
-        """
         
         all_classes = np.arange(start=0, stop=opt.nclass_all - 1, step=1)
 
@@ -162,80 +129,4 @@ class DATA_LOADER(object):
         return batch_feature, batch_att
 
 
-    def read_matdataset_v1(self, opt):
-        matcontent = sio.loadmat(opt.dataroot + "/" + opt.dataset + "/" + opt.image_embedding + ".mat")
-        feature = matcontent['features'].T
-        label = matcontent['labels'].astype(int).squeeze() - 1
-        matcontent = sio.loadmat(opt.dataroot + "/" + opt.dataset + "/" + opt.class_embedding + "_splits.mat")
-        trainval_loc = matcontent['trainval_loc'].squeeze() - 1
-        train_loc = matcontent['train_loc'].squeeze() - 1
-        val_unseen_loc = matcontent['val_loc'].squeeze() - 1
-        test_seen_loc = matcontent['test_seen_loc'].squeeze() - 1
-        test_unseen_loc = matcontent['test_unseen_loc'].squeeze() - 1    
-
-        self.attribute = torch.from_numpy(matcontent['att'].T).float()
-        self.attribute /= self.attribute.pow(2).sum(1).sqrt().unsqueeze(1).expand(self.attribute.size(0),self.attribute.size(1))
-
-        if not opt.validation:
-            if opt.preprocessing:
-                if opt.standardization:
-                    print('standardization...')
-                    scaler = preprocessing.StandardScaler()
-                else:
-                    scaler = preprocessing.MinMaxScaler()
-                
-                _train_feature = scaler.fit_transform(feature[trainval_loc])
-                _test_seen_feature = scaler.transform(feature[test_seen_loc])
-                _test_unseen_feature = scaler.transform(feature[test_unseen_loc])
-                self.train_feature = torch.from_numpy(_train_feature).float()
-                mx = self.train_feature.max()
-                self.train_feature.mul_(1/mx)
-                self.train_label = torch.from_numpy(label[trainval_loc]).long() 
-                self.test_unseen_feature = torch.from_numpy(_test_unseen_feature).float()
-                self.test_unseen_feature.mul_(1/mx)
-                self.test_unseen_label = torch.from_numpy(label[test_unseen_loc]).long() 
-                self.test_seen_feature = torch.from_numpy(_test_seen_feature).float() 
-                self.test_seen_feature.mul_(1/mx)
-                self.test_seen_label = torch.from_numpy(label[test_seen_loc]).long()
-
-            else:
-                self.train_feature = torch.from_numpy(feature[trainval_loc]).float()
-                self.train_label = torch.from_numpy(label[trainval_loc]).long() 
-                self.test_unseen_feature = torch.from_numpy(feature[test_unseen_loc]).float()
-                self.test_unseen_label = torch.from_numpy(label[test_unseen_loc]).long() 
-                self.test_seen_feature = torch.from_numpy(feature[test_seen_loc]).float() 
-                self.test_seen_label = torch.from_numpy(label[test_seen_loc]).long()
-        else:
-            self.train_feature = torch.from_numpy(feature[train_loc]).float()
-            self.train_label = torch.from_numpy(label[train_loc]).long()
-            self.test_unseen_feature = torch.from_numpy(feature[val_unseen_loc]).float()
-            self.test_unseen_label = torch.from_numpy(label[val_unseen_loc]).long() 
-    
-        print("train_feature: ", self.train_feature.size()[0])
-
-        self.seenclasses = torch.from_numpy(np.unique(self.train_label.numpy()))
-        self.unseenclasses = torch.from_numpy(np.unique(self.test_unseen_label.numpy()))
-
-        test_unseen_loc = get_where(label, np.unique(self.test_unseen_label))
-        self.test_unseen_feature = torch.from_numpy(feature[test_unseen_loc]).float()
-        self.test_unseen_label = torch.from_numpy(label[test_unseen_loc]).long()
-
-        test_train = get_where(label, np.unique(self.test_seen_label))
-        
-        train, test = self.split_train_test(test_train, label)
-        self.train_label = torch.from_numpy(label[train]).long()
-        print("train: ", train.shape)
-
-        self.train_feature = torch.from_numpy(feature[train]).float()
-        self.test_seen_feature = torch.from_numpy(feature[test]).float()
-        self.test_seen_label = torch.from_numpy(label[test]).long()
-
-        self.ntrain = self.train_feature.size()[0]
-        print("ntrain: ", self.ntrain)
-        self.ntest_seen = self.test_seen_feature.size()[0]
-        self.ntest_unseen = self.test_unseen_feature.size()[0]
-        self.ntrain_class = self.seenclasses.size(0)
-        self.ntest_class = self.unseenclasses.size(0)
-        self.train_class = self.seenclasses.clone()
-        self.allclasses = torch.arange(0, self.ntrain_class+self.ntest_class).long()
-        self.train_mapped_label = map_label(self.train_label, self.seenclasses) 
+   
